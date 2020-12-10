5dc483a7322041dab121b40048ca4af3fcc27778
Hakan Önal <hakan.onal@yahoo.com>
2020-12-10 11:21:36 +0300

    Trying to make work vue swatches not working yet.

---
+++ src/components/Digit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form__field">
+    <div class="form__label">
+      <strong>Please choose a color:</strong>
+    </div>
+    <div class="form__input" v-if="color">
+      <VSwatches
+        v-model="color"
+        :swatches="swatches"
+        row-length="6"
+        shapes="circles"
+        show-border
+        popover-x="left"
+      ></VSwatches>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import VSwatches from 'vue-swatches'
+
+export default {
+  components: {
+    VSwatches
+  },
+  data () {
+    return {
+      color: '#F64272',
+      swatches: ['#F64272', '#F6648B', '#F493A7', '#F891A6', '#FFCCD5', '']
+    }
+  }
+}
+</script>
--- src/components/Mastermind.vue
+++ src/components/Mastermind.vue
@@ -1,36 +1,20 @@
 <template>
   <div class="hello">
     <h1> Mastermind </h1>
+    <Digit/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import Digit from '@/components/Digit.vue'
 
 @Options({
-  props: {
-    msg: String
+  components: {
+    Digit
   }
 })
 export default class HelloWorld extends Vue {
-  msg!: string
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
